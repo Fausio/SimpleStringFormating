84ec4c6c249edf8a85b31627f9b97b2f05ebcdf5
fausio luis <fausioluis@live.com>
2020-03-16 10:38:30 +0200

    Add project files.

---
+++ ConsoleCoreSimpleStringFormating/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleCoreSimpleStringFormating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add the first number");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now add the secund number");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a + b;
+
+            Console.WriteLine("the sum of {0} and {1} is: {2}", a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
